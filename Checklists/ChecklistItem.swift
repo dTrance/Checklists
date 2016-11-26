e6f0fc8ea653913580abd584bf54250aeb893086
Doug Trance <dtrance@dtcc.edu>
2016-11-26 14:17:02 -0500

    Cleaned up lists and added an array to hold the checklist objects.

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Doug Trance on 11/26/16.
+//  Copyright © 2016 Trance Apps. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+}
