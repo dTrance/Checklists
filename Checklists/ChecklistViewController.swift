521700108c679d763e7a12583e165635d0e59ab9
Doug Trance <dtrance@dtcc.edu>
2016-11-26 10:28:43 -0500

    Simple app to learn tableviews

---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Doug Trance on 11/26/16.
+//  Copyright © 2016 Trance Apps. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
